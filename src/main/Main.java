c628c7786ec9c6617876a13a481b7747762bc71b
daniel <danielmito6463@gmail.com>
2025-09-24 16:47:49 -0300

    criei uma parte da janela, o tamanho dela e o titulo da janel, ainda p terminar

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+import javax.swing.JFrame;
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Tubarões Vs Orcas");
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+
+    }
+}
