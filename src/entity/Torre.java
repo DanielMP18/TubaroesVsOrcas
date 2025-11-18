fd0434dbcef87b4f3b3e4334bc307b21aee6f2cb
Boscocav22 <jboscofotografia@gmail.com>
2025-11-18 15:24:36 -0300

    Implement upgrade logic and specialization in Torre
    Added upgrade logic and element specialization to Torre class.

--- src/entity/Torre.java
+++ src/entity/Torre.java
@@ -1,5 +1,6 @@
 package entity;
 
+import game.Elemento; 
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.util.List;
@@ -17,19 +18,28 @@ public abstract class Torre {
 
     protected List<Inimigo> inimigos;
     protected List<Projetil> projeteis;
+    
+    // --- Variáveis de Upgrade ---
+    protected Elemento elemento;
+    protected int nivel;
+    protected int custoUpgrade;
+    protected int danoBase; 
 
     public Torre(int col, int row, int tamanho, List<Inimigo> inimigos, List<Projetil> projeteis) {
         this.col = col;
         this.row = row;
         this.tamanho = tamanho;
-        this.x = col * tamanho + tamanho / 2;
-        this.y = row * tamanho + tamanho / 2;
+        this.x = col * tamanho + tamanho / 2; // Centro da torre
+        this.y = row * tamanho + tamanho / 2; // Centro da torre
         this.inimigos = inimigos;
         this.projeteis = projeteis;
         this.ultimoDisparo = 0;
         this.alvo = null;
+        
+        
+        this.elemento = Elemento.NEUTRO; 
+        this.nivel = 1;
     }
-
 
     protected void encontrarAlvo() {
         Inimigo inimigoMaisProximo = null;
@@ -47,9 +57,11 @@ public abstract class Torre {
         this.alvo = inimigoMaisProximo;
     }
 
-
     public void update() {
-        encontrarAlvo();
+        if (alvo == null || !alvo.isAtivo() || Point.distance(this.x, this.y, alvo.getX(), alvo.getY()) > this.alcance) {
+             encontrarAlvo();
+        }
+       
         long agora = System.nanoTime();
         if (alvo != null && (agora - ultimoDisparo) > cadenciaDeTiro) {
             atirar();
@@ -57,19 +69,37 @@ public abstract class Torre {
         }
     }
 
+    // --- LÓGICA DE UPGRADE (EFEITOS DE STATUS) ---
+    public void especializar(Elemento novoElemento) {
+        if (this.nivel != 1 || this.elemento != Elemento.NEUTRO) {
+            System.out.println("Esta torre já foi especializada!");
+            return;
+        }
+
+        this.elemento = novoElemento; !
+        this.nivel = 2; 
+
+        // Bônus geral por evoluir
+        this.danoBase = (int) (this.danoBase * 1.2); // +20% dano
+        this.alcance = (int) (this.alcance * 1.1); // +10% alcance
+        
+        this.custoUpgrade = this.custo * 2; // Custo do Nível 3
+        System.out.println("Torre especializada em " + novoElemento);
+    }
+    
     public abstract void draw(Graphics2D g2);
 
     protected abstract void atirar();
 
-    public int getCusto() {
-        return custo;
-    }
-
-    public int getCol() {
-        return col;
-    }
-
-    public int getRow() {
-        return row;
-    }
+    // --- Getters ---
+    public int getCusto() { return custo; }
+    public int getCol() { return col; }
+    public int getRow() { return row; }
+    public Elemento getElemento() { return elemento; }
+    public int getNivel() { return nivel; }
+    public int getCustoUpgrade() { return custoUpgrade; }
+    public boolean isEspecializada() { return this.elemento != Elemento.NEUTRO; }
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getAlcance() { return alcance; }
 }
