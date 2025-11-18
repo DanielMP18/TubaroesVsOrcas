64501ef56d86c660fa8ce573275ce8a2771954c1
Boscocav22 <jboscofotografia@gmail.com>
2025-11-18 15:25:39 -0300

    Add elemental attribute to TorreCanhao projectiles

--- src/entity/TorreCanhao.java
+++ src/entity/TorreCanhao.java
@@ -10,14 +10,19 @@ public class TorreCanhao extends Torre {
 
     public TorreCanhao(int col, int row, int tamanho, List<Inimigo> inimigos, List<Projetil> projeteis) {
         super(col, row, tamanho, inimigos, projeteis);
+        
+        // Stats Base (Nível 1)
         this.custo = CUSTO;
         this.alcance = 180;
         this.cadenciaDeTiro = 800_000_000L; // 0.8 segundos
+        this.danoBase = 25; 
+        this.custoUpgrade = 75; // Custo Nv1 -> Nv2
     }
 
     @Override
     protected void atirar() {
-        Projetil p = new Projetil(x, y, 8f, 25, alvo, Color.ORANGE);
+        // Passa o elemento da torre (que será NEUTRO ou FOGO/AGUA/ALGA)
+        Projetil p = new Projetil(x, y, 8f, this.danoBase, alvo, Color.ORANGE, this.elemento);
         projeteis.add(p);
     }
 
@@ -25,10 +30,7 @@ public class TorreCanhao extends Torre {
     public void draw(Graphics2D g2) {
         g2.setColor(new Color(0, 150, 0));
         g2.fillOval(col * tamanho, row * tamanho, tamanho, tamanho);
-
         g2.setColor(Color.GRAY);
         g2.fillRect(x - 5, y - 5, 10, 10);
     }
-
-
 }
