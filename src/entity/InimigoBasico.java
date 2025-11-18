1b16dcb3eaeedc0df134c7dcad85a2079e3e639b
Boscocav22 <jboscofotografia@gmail.com>
2025-11-18 15:21:40 -0300

    Set elemento to FOGO in InimigoBasico constructor

--- src/entity/InimigoBasico.java
+++ src/entity/InimigoBasico.java
@@ -1,18 +1,17 @@
 package entity;
 
+import game.Elemento;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.util.List;
 
-
 public class InimigoBasico extends Inimigo {
-
 
     public InimigoBasico(float x, float y, List<Point> caminho) {
         super(x, y, caminho, 100, 1.5f, 15, 32, 32);
+        this.elemento = Elemento.FOGO; // <--- Elemento
     }
-
 
     @Override
     public void drawInimigo(Graphics2D g2) {
