65317a107868d4b52924907b45c57210c51c8c8f
Boscocav22 <jboscofotografia@gmail.com>
2025-11-18 15:23:22 -0300

    Add elemento property to InimigoTank class

--- src/entity/InimigoTank.java
+++ src/entity/InimigoTank.java
@@ -1,5 +1,6 @@
 package entity;
 
+import game.Elemento;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
@@ -9,15 +10,13 @@ public class InimigoTank extends Inimigo {
 
     public InimigoTank(float x, float y, List<Point> caminho) {
         super(x, y, caminho, 500, 0.8f, 50, 40, 40);
+        this.elemento = Elemento.AGUA; // <--- Elemento
     }
 
     @Override
     public void drawInimigo(Graphics2D g2) {
-
         g2.setColor(Color.BLACK);
         g2.fillRect((int) x, (int) y, largura, altura);
-
-
         g2.setColor(Color.DARK_GRAY);
         g2.drawRect((int) x + 2, (int) y + 2, largura - 5, altura - 5);
     }
