2cc42d3608a9a9a05b2a12796f59d86a2902272a
Boscocav22 <jboscofotografia@gmail.com>
2025-11-18 15:23:55 -0300

    Add elemento parameter to Projetil constructor

--- src/entity/Projetil.java
+++ src/entity/Projetil.java
@@ -1,5 +1,6 @@
 package entity;
 
+import game.Elemento; // Importar o Enum
 import java.awt.Color;
 import java.awt.Graphics2D;
 
@@ -11,22 +12,26 @@ public class Projetil {
     private Inimigo alvo;
     private boolean ativo = true;
     private Color cor;
+    
+    private Elemento elemento; // Armazena o elemento do projétil
 
-    public Projetil(float x, float y, float velocidade, int dano, Inimigo alvo, Color cor) {
+    public Projetil(float x, float y, float velocidade, int dano, Inimigo alvo, Color cor, Elemento elemento) {
         this.x = x;
         this.y = y;
         this.velocidade = velocidade;
         this.dano = dano;
         this.alvo = alvo;
         this.cor = cor;
+        this.elemento = elemento; // Armazena o elemento
     }
 
     public void update() {
         if (!ativo || alvo == null || !alvo.isAtivo()) {
             desativar();
             return;
-
         }
+        
+        // Mira no centro do inimigo
         float alvoX = alvo.getX() + ((float) alvo.getLargura() / 2);
         float alvoY = alvo.getY() + ((float) alvo.getAltura() / 2);
 
@@ -35,8 +40,10 @@ public class Projetil {
         x += velocidade * Math.cos(angulo);
         y += velocidade * Math.sin(angulo);
 
+        // Checa colisão
         if (Math.sqrt(Math.pow(alvoX - x, 2) + Math.pow(alvoY - y, 2)) < velocidade) {
-            alvo.levarDano(dano);
+            // --- APLICA O DANO E O EFEITO ---
+            alvo.levarDano(dano, this.elemento); 
             desativar();
         }
     }
@@ -46,11 +53,6 @@ public class Projetil {
         g2.fillOval((int) x - 4, (int) y - 4, 8, 8);
     }
 
-    public void desativar() {
-        this.ativo = false;
-    }
-
-    public boolean isAtivo() {
-        return ativo;
-    }
+    public void desativar() { this.ativo = false; }
+    public boolean isAtivo() { return ativo; }
 }
